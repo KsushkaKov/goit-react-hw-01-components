f6a3d3ccd97b9454bdd4b6f14cb9cc704a6b885d
Ksenia Kovalenko <ksushka.ua@gmail.com>
2022-12-13 22:55:53 +0200

    done task 3

--- src/index.js
+++ src/index.js
@@ -3,19 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
-import { Profile } from 'components/Profile/Profile';
-import user from './components/Profile/user.json';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-    </App>
+    <App />
   </React.StrictMode>
 );

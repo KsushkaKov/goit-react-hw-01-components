b6715c747ccef50623ef837d7749571dc3370bfa
Ksenia Kovalenko <ksushka.ua@gmail.com>
2022-12-13 23:34:05 +0200

    task 4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import user from './Profile/user.json';
 import data from './Statistics/data.json';
 import friends from './FriendList/friends.json';
+import transactions from './TransactionHistory/transactions.json';
 
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { Section } from './Section/Section';
 import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
 export const App = () => {
   return (
@@ -20,6 +22,7 @@ export const App = () => {
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </Section>
   );
 };
